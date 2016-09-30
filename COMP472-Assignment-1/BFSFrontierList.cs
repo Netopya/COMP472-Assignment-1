206a3c375f21e2252e3d17f6f157b83be67026ae
Netopya <netopya@netopyaplanet.com>
2016-09-30 16:13:49 -0400

    Implemented custom list for BFS
    Part implementation for #3

---
+++ COMP472-Assignment-1/BFSFrontierList.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMP472_Assignment_1
+{
+    class BFSFrontierList : IFrontier
+    {
+        Queue<IBranch> list;
+
+        public void Add(IBranch branch)
+        {
+            list.Enqueue(branch);
+        }
+
+        public IBranch GetNext()
+        {
+            return list.Dequeue();
+        }
+    }
+}
---
+++ COMP472-Assignment-1/IFrontier.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMP472_Assignment_1
+{
+    interface IFrontier
+    {
+        void Add(IBranch branch);
+        IBranch GetNext();
+    }
+}
